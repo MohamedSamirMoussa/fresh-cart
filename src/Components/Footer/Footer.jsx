37e2f4069a16f3d0b359f82f2355f8a2aaa76a2a
MohammedSamirMousa <mosamir5000@gmail.com>
2025-02-03 08:48:17 +0200

    fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-wrap items-center justify-center flex-row border-b border-gray-300 gap-x-5 py-10">
           <div className="w-full md:w-[65%] lg:w-[70%]">
-          <input type="email" placeholder="Enter your email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full" />
+          <input type="email" placeholder="Enter your email" className="bg-gray-50 border px-4 py-2 md:px-4 md:py-2 outline-[#0aad0a]  border-gray-300 text-gray-900 text-sm rounded-lg block w-full" />
           </div>
           <div className="md:w-[30%] lg:w-[25%] my-2 md:my-0">
             <button className="bg-[#0aad0a] text-white px-4 py-2 md:px-4 md:py-2 rounded-lg">Share App Link</button>
