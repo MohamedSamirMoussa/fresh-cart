d10acd8094748f5ddc79241d93e540d56fe02ea1
MohammedSamirMousa <mosamir5000@gmail.com>
2025-02-03 07:42:29 +0200

    شسي

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
-import '../node_modules/flowbite/dist/flowbite.js'
+import 'flowbite/dist/flowbite.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
