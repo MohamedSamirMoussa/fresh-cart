69e9e36180c38cd145b6016d74b7e47f42ad4d7f
MohammedSamirMousa <mosamir5000@gmail.com>
2025-02-03 09:03:04 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
-import 'flowbite'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
@@ -14,5 +14,5 @@ export default {
       xl: "1280px",
     },
   },
-  plugins: [ flowbite.plugin(),],
+  plugins: ["flowbite/plugin"],
 };
