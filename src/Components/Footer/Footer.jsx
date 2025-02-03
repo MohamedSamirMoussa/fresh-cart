0eb20c86e75149294a4a113a1005a672a3b83bae
MohammedSamirMousa <mosamir5000@gmail.com>
2025-02-03 07:51:24 +0200

    asdasda

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,16 +10,16 @@ const Footer = () => {
             we will send you a link, open it on your phone to download the app
           </p>
         </div>
-        <div className="flex flex-wrap items-center justify-center flex-row border-b border-gray-300 gap-x-2 py-10">
-          <div className="w-full md:w-[70%] lg:w-[70%]">
-          <input type="email" placeholder="Enter your email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2" />
+        <div className="flex flex-wrap items-center justify-center flex-row border-b border-gray-300 gap-x-5 py-10">
+          <div className="w-full md:w-[65%] lg:w-[70%]">
+          <input type="email" placeholder="Enter your email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full" />
           </div>
-          <div className="md:w-[25%] my-2 md:my-0">
-            <button className="bg-[#0aad0a] text-white px-4 py-2 md:px-8 md:py-2 rounded-lg">Share App Link</button>
+          <div className="md:w-[30%] lg:w-[25%] my-2 md:my-0">
+            <button className="bg-[#0aad0a] text-white px-4 py-2 md:px-4 md:py-2 rounded-lg">Share App Link</button>
           </div>
    
         </div>
-        <div className="flex flex-wrap items-center space-x-3 py-10 border-b border-gray-300">
+        <div className="flex flex-wrap justify-center md:justify-baseline items-center space-x-3 py-10 border-b border-gray-300">
           <h3 className="text-lg text-gray-500 font-light">Payment Partners</h3>
           <ul className="flex items-center justify-center space-x-2">
             <li>
@@ -32,7 +32,7 @@ const Footer = () => {
               <i className="fa-brands fa-cc-mastercard fa-2x"></i>
             </li>
           </ul>
-          <div className="flex items-center ml-auto space-x-3">
+          <div className="flex items-center md:ml-auto space-x-3">
             <h3 className="text-lg text-gray-500 font-light">
               Get deliveries with FreshCart
             </h3>
