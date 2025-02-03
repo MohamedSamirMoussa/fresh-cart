3a5bfc0705b792207694d1150a745deb649bbd76
MohammedSamirMousa <mosamir5000@gmail.com>
2025-02-03 09:49:05 +0200

    sadasdas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import 'flowbite';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
+import './../node_modules/flowbite/dist/flowbite.min.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
