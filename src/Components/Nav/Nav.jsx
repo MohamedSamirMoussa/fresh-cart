3b63764ee0fb0f08c58a0e8a06e17fb758f0f466
MohammedSamirMousa <mosamir5000@gmail.com>
2025-02-21 20:52:31 +0200

    fix

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -10,21 +10,14 @@ import { WishListContext } from "../../Context/WishListContext";
 
 
 // const { numOfItem } = useContext(CartContext)
-// const [touched, setTouched] = useState(false)
+
 // const [nav, setNav] = useState(false)
 // const { token, setToken } = useContext(AuthContext)
 // const { wishListNum } = useContext(WishListContext)
 // const navigate = useNavigate()
 
 
-//     function touchBtn() {
 
-//         if (touched === false) {
-//             setTouched(true)
-//         } else {
-//             setTouched(false)
-//         }
-//     }
 
 //     const handleNav = () => {
 //         if (window.scrollY >= 10) {
@@ -51,7 +44,7 @@ import { WishListContext } from "../../Context/WishListContext";
 //                         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#0aad0a] focus:ring-offset-2 focus:ring-offset-gray-50"
 //                         aria-controls="navbar-default"
 //                         aria-expanded="false"
-//                         onClick={touchBtn}
+//                         
 //                     >
 //                         <span className="sr-only">Open main menu</span>
 //                         <svg
@@ -137,11 +130,12 @@ import { WishListContext } from "../../Context/WishListContext";
 
 
 const Nav = () => {
-    const { numOfItem } = useContext(CartContext)
     const [nav, setNav] = useState(false)
+    const [touched, setTouched] = useState(false)
+    const navigate = useNavigate()
+    const { numOfItem } = useContext(CartContext)
     const { token, setToken } = useContext(AuthContext)
     const { wishListNum } = useContext(WishListContext)
-    const navigate = useNavigate()
     const logout = () => {
         localStorage.removeItem("tkn")
         setToken(null)
@@ -155,6 +149,14 @@ const Nav = () => {
         }
     }
     window.addEventListener("scroll", handleNav)
+    function touchBtn() {
+
+        if (touched === false) {
+            setTouched(true)
+        } else {
+            setTouched(false)
+        }
+    }
     return (
 
 
@@ -190,7 +192,7 @@ const Nav = () => {
                         <button className="text-gray-600 mx-3 cursor-pointer md:hover:text-[#000] transition-all duration-500 " onClick={logout}>
                             Log out
                         </button>}
-                    <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-sticky" aria-expanded="false">
+                    <button onClick={touchBtn} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
