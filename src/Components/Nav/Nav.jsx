b74ddf372ff290552b65d2f4536f46d056c96421
MohammedSamirMousa <mosamir5000@gmail.com>
2025-03-02 02:21:31 +0200

    fix

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -40,8 +40,8 @@ const Nav = () => {
         <nav className={nav ? "bg-white shadow-xl fixed w-full z-20 top-0 start-0 border-b border-gray-200 py-2 transition-all duration-500"
             : "bg-white shadow-xl fixed w-full z-20 top-0 start-0 border-b border-gray-200 transition-all duration-500"
         }>
-            <div className={nav ? "lg:w-[80%] transition-all duration-500 flex flex-wrap items-center justify-between mx-auto p-4"
-                : "flex flex-wrap items-center justify-between mx-auto p-4 lg:w-[90%] transition-all duration-500"
+            <div className={nav ? "lg:w-[90%] transition-all duration-500 flex flex-wrap items-center justify-between mx-auto p-4"
+                : "flex flex-wrap items-center justify-between mx-auto p-4 lg:w-[100%] transition-all duration-500"
             }>
                 <Link
                     to={"/"}
@@ -62,6 +62,8 @@ const Nav = () => {
                                 <i className="fa-solid fa-heart text-red-700 fa-lg"></i>
                                 <div className="absolute inline-flex items-center justify-center w-[20px] h-[20px] text-[10px] font-bold text-white bg-red-500 border-2  rounded-full -top-0 md:-top-2 -end-4">{wishListNum}</div>
                             </NavLink>
+
+
                         </> : ""
                     }
                     {!token ? <>
@@ -72,18 +74,22 @@ const Nav = () => {
                         <button className="text-gray-600 mx-1.5 cursor-pointer md:hover:text-[#000] transition-all duration-500 " onClick={logout}>
                             Logout
                         </button>}
-                    <button onClick={touchBtn} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-sticky" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
+
+                    {
+                        token ? <button onClick={touchBtn} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-sticky" aria-expanded="false">
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 1h15M1 7h15M1 13h15" />
+                            </svg>
+                        </button> : ""
+                    }
+
                 </div>
 
 
 
 
-                <div className={!touched ? "items-center justify-center hidden md:hidden lg:flex md:w-full lg:w-auto lg:order-2" : "items-center justify-center md:hidden md:w-full lg:order-1 lg:flex"} id="navbar-sticky">
+                <div className={!touched ? "items-center justify-center hidden md:hidden lg:flex w-full lg:w-auto lg:order-2" : "items-center justify-center w-full lg:order-1 lg:flex"} id="navbar-sticky">
                     <ul className="flex flex-col items-center p-4 md:p-0 mt-4 font-medium rounded-lg  lg:space-x-8 lg:flex-row lg:mt-0 md:border-0 md:bg-white">
                         {token ? <><li>
                             <NavLink
