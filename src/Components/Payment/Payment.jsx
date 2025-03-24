27f664ce9fcba89231788839807710f0c3a44dd3
MohammedSamirMousa <mosamir5000@gmail.com>
2025-03-24 19:54:13 +0200

    fix

--- src/Components/Payment/Payment.jsx
+++ src/Components/Payment/Payment.jsx
@@ -60,7 +60,7 @@ const Payment = () => {
                         },
                         params: {
                             // My URL
-                            url: "https://mohamedsamirmoussa.github.io/fresh-cart/#"
+                            url: "https://fresh-cart-steel-pi.vercel.app/#"
                         }
                     }
                 )
